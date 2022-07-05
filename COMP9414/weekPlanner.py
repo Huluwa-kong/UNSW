97fbd13957b442ad7119769bfa297c7295cd0af4
Brye <69114816+Huluwa-kong@users.noreply.github.com>
2022-07-05 08:50:21 +0800

    Update weekPlanner.py

--- COMP9414/weekPlanner.py
+++ COMP9414/weekPlanner.py
@@ -1,21 +1,31 @@
-# activities with name and duration
-activity hnamei hdurationi
-# binary constraints
-constraint hA1i before hA2i # A1 ends when or before A2 starts
-constraint hA1i after hA2i # A1 starts after or when A2 ends
-constraint hA1i starts hA2i # A1 and A2 start at the same day and time
-constraint hA1i ends hA2i # A1 and A2 end at the same day and time
-constraint hA1i overlaps hA2i # A2 starts after A1 starts and not after A1 ends,
-# and ends after A1 ends
-constraint hA1i during hA2i # A1 starts after A2 starts and ends before A2 ends
-constraint hA1i equals hA2i # A1 and A2 start and end at the same day and time
-constraint hA1i same-day hA2i # A1 and A2 start and end on the same day
+# ================== weekPlanner.py ==================
 
-# hard domain constraints
-domain hAi on hdi # A starts (and ends) on day d
-domain hAi before hdi # A starts (and ends) before day d
-domain hAi after hdi # A starts (and ends) after day d
-domain hAi starts-before hti # A starts at or before time t on any day
-domain hAi starts-after hti # A starts at or after time t on any day
-domain hAi ends-before hti # A ends at or before time t on any day
-domain hAi ends-after hti # A ends on or after time t on any day
+"""
+weekPlanner.py
+weekPlanner.py
+weekPlanner.py
+如有需要，请联系v codingtutor或者q 122929048
+"""
+
+"""
+解答和提示：
+1. 首先定义好所有的 binary constraints 和 hard domain constraints，此部分代码较多，例如
+
+def starts(x, y):
+    """Check if time x and y is at same day and time"""
+    return x == y
+
+# 其他constraints
+...
+
+2. 从文件中读取Variable，constraint以及所有的soft domain constraints，构建CSP
+
+3. 从soft domain constraints构建Heuristic
+
+4. 从AStarSearcher extend your Searcher，该searcher接受一个Heuristic，在搜索过程中会用到
+
+
+
+
+"""
+
